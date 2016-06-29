a39d366c01737565874f14f520970e94bf618fab
lym <lym@qq.com>
2016-06-29 21:57:31 +0800

    1.搭建cocoa pod环境，删除storyboard，新建项目框架目录

---
+++ ifanr/ifanr/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  ifanr
+//
+//  Created by 梁亦明 on 16/6/29.
+//  Copyright © 2016年 ifanrOrg. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+    }
+}
