96589bddd5afb3594cde1ab05697516db846d83a
lyimin <1142343535@qq.com>
2016-07-25 22:11:15 +0800

    集成ShareSDK

--- ifanr/ifanr/ifanr-Bridging-Header.h
+++ ifanr/ifanr/ifanr-Bridging-Header.h
@@ -11,10 +11,5 @@
 
 #import <ShareSDK/ShareSDK.h>
 #import <ShareSDKConnector/ShareSDKConnector.h>
-//腾讯SDK头文件
-#import <TencentOpenAPI/TencentOAuth.h>
-#import <TencentOpenAPI/QQApiInterface.h>
 //微信SDK头文件
 #import "WXApi.h"
-//新浪微博SDK头文件
-#import "WeiboSDK.h"
